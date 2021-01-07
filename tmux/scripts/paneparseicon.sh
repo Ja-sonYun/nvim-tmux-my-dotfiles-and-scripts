ad958f1f5436cfd907f5f99827650f564c669a8f
Jason yun <jasonyun@Jasons-MacBook-Air.local>
2021-01-08 01:58:18 +0900

    committed via script

--- tmux/scripts/paneparseicon.sh
+++ tmux/scripts/paneparseicon.sh
@@ -2,14 +2,26 @@ ARG=$1
 ISVIM=""
 NOTVIM=0
 NOTBON=0
-if [[ $ARG == *"NVIM" ]]; then
-    ISVIM=" "
+LA=""
+# shell.sh + (~/.s/dwd) - NVIM
+if [[ $ARG == *"NVIM"* ]]; then
+    if [[ $ARG == *" + ("* ]]; then
+        ISVIM=" "
+        LA=" #[fg=colour196]#[fg=default]"
+    else
+        ISVIM=" "
+        LA=" "
+    fi
 else
     NOTVIM=1
 fi
 
+
 arrIN=(${ARG/ - / })
 INF=${arrIN[1]}
+if [ $NOTVIM -eq 0 ] && [ "${arrIN[1]}" == "+" ]; then
+    INF=${arrIN[2]}
+fi
 # PWD="${arrIN[2]/NVIM/}"
 
 OUTPUT="$ISVIM$arrIN"
@@ -24,7 +36,8 @@ else
 fi
 
 if [ $NOTVIM -eq 1 ] && [ $NOTBON -eq 1 ]; then
-    echo " no info"
+    # echo " no info"
+    echo $1
 else
-    echo $OUTPUT
+    echo $OUTPUT$LA
 fi
